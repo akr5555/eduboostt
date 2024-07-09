16add582afdc8a46c3eacba946c5b7ccea037ea5
akr5555 <akakakraj12345@gmail.com>
2024-07-09 15:02:19 +0530

    Update notice-controller.js

--- server/controllers/notice-controller.js
+++ server/controllers/notice-controller.js
@@ -1,13 +1,13 @@
-const Notice = require('../models/noticeSchema.js');
+import Notice from '../models/noticeSchema.js';
 
 const noticeCreate = async (req, res) => {
     try {
         const notice = new Notice({
             ...req.body,
-            school: req.body.adminID
-        })
-        const result = await notice.save()
-        res.send(result)
+            school: req.body.adminID  // Assuming adminID is part of the request body
+        });
+        const result = await notice.save();
+        res.send(result);
     } catch (err) {
         res.status(500).json(err);
     }
@@ -15,9 +15,9 @@ const noticeCreate = async (req, res) => {
 
 const noticeList = async (req, res) => {
     try {
-        let notices = await Notice.find({ school: req.params.id })
+        let notices = await Notice.find({ school: req.params.id });
         if (notices.length > 0) {
-            res.send(notices)
+            res.send(notices);
         } else {
             res.send({ message: "No notices found" });
         }
@@ -30,33 +30,33 @@ const updateNotice = async (req, res) => {
     try {
         const result = await Notice.findByIdAndUpdate(req.params.id,
             { $set: req.body },
-            { new: true })
-        res.send(result)
+            { new: true });
+        res.send(result);
     } catch (error) {
         res.status(500).json(error);
     }
-}
+};
 
 const deleteNotice = async (req, res) => {
     try {
-        const result = await Notice.findByIdAndDelete(req.params.id)
-        res.send(result)
+        const result = await Notice.findByIdAndDelete(req.params.id);
+        res.send(result);
     } catch (error) {
         res.status(500).json(err);
     }
-}
+};
 
 const deleteNotices = async (req, res) => {
     try {
-        const result = await Notice.deleteMany({ school: req.params.id })
+        const result = await Notice.deleteMany({ school: req.params.id });
         if (result.deletedCount === 0) {
-            res.send({ message: "No notices found to delete" })
+            res.send({ message: "No notices found to delete" });
         } else {
-            res.send(result)
+            res.send(result);
         }
     } catch (error) {
         res.status(500).json(err);
     }
-}
+};
 
-module.exports = { noticeCreate, noticeList, updateNotice, deleteNotice, deleteNotices };
+export { noticeCreate, noticeList, updateNotice, deleteNotice, deleteNotices };
