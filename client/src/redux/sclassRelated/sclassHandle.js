a32365728e45357dbb9e2b419dbae07746b2f818
akr5555 <akakakraj12345@gmail.com>
2024-07-09 19:09:52 +0530

    Update sclassHandle.js

--- client/src/redux/sclassRelated/sclassHandle.js
+++ client/src/redux/sclassRelated/sclassHandle.js
@@ -11,7 +11,7 @@ import {
     getSubDetailsSuccess,
     getSubDetailsRequest
 } from './sclassSlice';
-const REACT_APP_BASE_URL = "http://localhost:3000";
+const REACT_APP_BASE_URL = process.env.REACT_APP_BASE_URL || 3000
 export const getAllSclasses = (id, address) => async (dispatch) => {
     dispatch(getRequest());
     
