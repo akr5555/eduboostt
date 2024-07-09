e11358cf62b9830463df2c7c11921a41dd25d8a0
akr5555 <akakakraj12345@gmail.com>
2024-07-09 18:14:11 +0530

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -1,11 +1,10 @@
 import express from 'express';
-import cors from "cors";
+import cors from 'cors'
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 // const bodyParser = require("body-parser")
-const app = express()
 import Routes from "./routes/route.js"
-import path from 'path';
+// import path from 'path';
 
 // const PORT = "http://localhost:3000";
 
@@ -13,19 +12,19 @@ dotenv.config();
 
 // app.use(bodyParser.json({ limit: '10mb', extended: true }))
 // app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
+const app = express();
 
 
-app.use(express.static(path.join(__dirname, '/client/build')));
+/* app.use(express.static(path.join(__dirname, '/client/build')));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, 'client/build/index.html'));
-});
+}); */
 
 
-app.use(express.json({ limit: '10mb' }))
-app.use(cors())
-
+app.use(express.json());
+app.use (cors());
 
 mongoose
     .connect(process.env.MONGO, {
@@ -41,6 +40,7 @@ app.use('/', Routes);
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
 });
+
 
 
 /* app.listen(PORT, () => {
