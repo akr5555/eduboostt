70a6c6dffe03f548b34267696167645deac7c0a5
akr5555 <akakakraj12345@gmail.com>
2024-07-09 21:39:04 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -25,8 +25,13 @@ mongoose
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
-    .then(console.log("Connected to MongoDB"))
-    .catch((err) => console.log("NOT CONNECTED TO NETWORK", err));
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+        console.log("NOT CONNECTED TO NETWORK", err);
+    });
+
 
 app.use(express.static(path.join(__dirname, '/client/build')));
 
