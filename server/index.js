a0adf644026d1fb03a4acaefc04ea371f9c10fc7
akr5555 <akakakraj12345@gmail.com>
2024-07-09 14:23:33 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const dotenv = require("dotenv")
 // const bodyParser = require("body-parser")
 const app = express()
 const Routes = require("./routes/route.js")
+import path from 'path';
 
 // const PORT = "http://localhost:3000";
 
@@ -12,12 +13,19 @@ dotenv.config();
 
 // app.use(bodyParser.json({ limit: '10mb', extended: true }))
 // app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
+const __dirname = path.resolve();
+
+
+app.use(express.static(path.join(__dirname, '/client/build')));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build/index.html'));
+});
+
 
 app.use(express.json({ limit: '10mb' }))
 app.use(cors())
-app.listen(3000, () => {
-  console.log("Server listening on port 3000");
-});
+
 
 mongoose
     .connect(process.env.MONGO, {
@@ -27,7 +35,12 @@ mongoose
     .then(console.log("Connected to MongoDB"))
     .catch((err) => console.log("NOT CONNECTED TO NETWORK", err))
 
+
 app.use('/', Routes);
+
+app.listen(3000, () => {
+  console.log("Server listening on port 3000");
+});
 
 
 /* app.listen(PORT, () => {
