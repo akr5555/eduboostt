eff3e480816ecd4dc2bcd755e58ecba829d9e86c
akr5555 <akakakraj12345@gmail.com>
2024-07-10 13:55:13 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,23 +4,33 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 // const bodyParser = require("body-parser")
 import Routes from "./routes/route.js"
-// import path from 'path';
+ import path from 'path';
 
  const PORT = process.env.PORT || 3000;
 
 dotenv.config();
 
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 // app.use(bodyParser.json({ limit: '10mb', extended: true }))
 // app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
-// const __dirname = path.resolve();
+
+const __dirname = path.resolve();
+
 const app = express();
 
 
-
-app.use(express.json());
+// app.use(express.json());
 // app.use (cors());
 
-mongoose
+/* mongoose
     .connect(process.env.MONGO, {
         useNewUrlParser: true,
         useUnifiedTopology: true
@@ -30,23 +40,37 @@ mongoose
     })
     .catch((err) => {
         console.log("NOT CONNECTED TO NETWORK", err);
-    });
+    }); */
 
 
-/* app.use(express.static(path.join(__dirname, '/client/build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build/index.html'));
-}); */
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
-app.use('/', Routes);
+app.use(express.json());
 
+app.use(cookieParser());
+
+app.listen(PORT, () => {
+    console.log(`Server started at port no. ${PORT}`)
+});
 /* app.listen(3000, () => {
   console.log("Server listening on port 3000");
 }); */
 
 
+app.use('/', Routes);
 
-app.listen(PORT, () => {
-    console.log(`Server started at port no. ${PORT}`)
-})
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode,
+  });
+});
+
+
