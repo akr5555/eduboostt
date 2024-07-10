daea566001a9896b673f8472459699145c580349
akr5555 <akakakraj12345@gmail.com>
2024-07-10 14:07:26 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -44,10 +44,10 @@ const app = express();
     }); */
 
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'client', 'build')));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname,'client', 'build', 'index.html'));
 });
 
 app.use(express.json());
